88050b4d6f910d51b40bd5b2b31f6be5ae8086cd
GRMagic <gustavo.rm90@gmail.com>
2019-12-16 14:19:43 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NTPClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Host: ");
+            var host = Console.ReadLine();
+            Console.WriteLine(NTPClient.GetNetworkTime(host));
+        }
+    }
+}
